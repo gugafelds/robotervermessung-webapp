f1294dce5683595ab889dfa75b8128d547995f9d
UmbertoB <umbertobarrosf@gmail.com>
2024-03-24 14:53:57 -0300

    Rename layout

--- src/app/trajectories/layout.tsx
+++ src/app/trajectories/layout.tsx
@@ -4,7 +4,7 @@ import { getTrajectoriesHeader } from '@/src/actions/trajectory.service';
 import { Sidebar } from '@/src/app/trajectories/components/Sidebar';
 import { TrajectoryProvider } from '@/src/providers/trajectory.provider';
 
-export default async function TrajectoriesPageLayout({
+export default async function TrajectoriesLayout({
   children,
 }: {
   children: React.ReactNode;
