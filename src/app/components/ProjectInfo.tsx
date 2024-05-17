edcef1d55b57c7c85a51aa87aa4689de841b0934
Barros <barros@lps.ruhr-uni-bochum.de>
2024-05-17 16:41:29 +0200

    change favicon and adjustments landing page

--- src/app/components/ProjectInfo.tsx
+++ src/app/components/ProjectInfo.tsx
@@ -2,46 +2,70 @@
 
 import Link from 'next/link';
 
-// import { ContactCard } from '@/src/app/components/ContactCard';
 import { Typography } from '@/src/components/Typography';
 
 export const ProjectInfo = () => {
   return (
-    <section className="mx-auto mt-8 max-w-xl px-8 md:max-w-2xl xl:max-w-6xl">
+    <section className="mx-auto mt-8 max-w-xl text-pretty px-8 md:max-w-2xl xl:max-w-4xl">
       <div className="mb-2 flex flex-col gap-3">
         <Typography as="h1">projektinfo</Typography>
-        <Typography as="h2" className="break-all">
+        <Typography as="h2">
           Autonome Messung und effiziente Speicherung von
           Industrieroboterbewegungsdaten
         </Typography>
       </div>
-      <div className="flex flex-col rounded-lg bg-gray-50 p-4 xl:flex-row xl:space-x-52">
-        <Typography as="h6" className="block">
-          Deutsche Forschungsgemeinschaft - DFG-47-1
+      <div className="mt-2">
+        <Typography as="p" className="mt-4">
+          Ziel dieses Projektes ist es, die Grundlagen für eine
+          Robotermessdatenbank zu schaffen, in der Bewegungsdaten von
+          Industrierobotern gespeichert werden können. Durch den Aufbau einer
+          umfassenden Datenbank, die kontinuierlich mit neuen Messdaten
+          angereichert werden kann, können verschiedene Anwendungen und
+          Szenarien unterstützt werden. Dadurch entfällt die Notwendigkeit von
+          zeitaufwändigen und teuren Einzelmessungen.
         </Typography>
+        <Typography as="p" className="mt-4">
+          Das Projekt startet im September 2023 und umfasst vier Meilensteine:
+          <li className="mt-4">
+            Entwicklung einer Methode für den Vergleich von Robotertrajektorien
+            unter Berücksichtigung von Abweichungen in der Position, der
+            Orientierung, der Geschwindigkeit und der Beschleunigung bei
+            verschiedenen Konfigurationen von Roboterarmen und unterschiedlichen
+            Abtastraten.
+          </li>
+          <li className="mt-2">
+            Aufbau einer Datenbank zur Speicherung und Bereitstellung von
+            Transaktionsdaten mit Schwerpunkt auf schnellem Datenzugriff und der
+            Möglichkeit, nach Teilsequenzen zu suchen.
+          </li>
+          <li className="mt-2">
+            Entwicklung einer Methode zur sequentiellen Versuchsplanung für
+            Roboterbahnmessungen, einschließlich der automatischen Generierung
+            von Roboterprogrammen für autonome Messungen.
+          </li>
+          <li className="mt-2">
+            Erfolgreiche Datengenerierung für die entwickelten Methoden und
+            Algorithmen zur autonomen Messung sowie Speicherung und
+            Bereitstellung der Messdaten
+          </li>
+        </Typography>
+        <Typography as="p" className="mt-5">
+          Diese Webseite dient als Remote-Anwendung mit direkter Visualisierung
+          der Daten, die in der MongoDB-Datenbank gespeichert sind.
+        </Typography>
+      </div>
+
+      <div className="mt-8 flex flex-col rounded-lg bg-gray-100 p-4">
         <Typography as="h6" className="block">
-          Projektnummer:{' '}
+          Das Forschungsprojekt wird von der Deutschen Forschungsgemeinschaft
+          (DFG) unter der Projektnummer{' '}
           <Link
             href="https://gepris.dfg.de/gepris/projekt/515675259"
             className="border-b-2 border-dotted border-b-black font-extrabold"
           >
             515675259
-          </Link>
-        </Typography>
-      </div>
-      <div className="mt-2">
-        <Typography as="p" className="text-justify">
-          In diesem Projekt soll die Grundlage für eine
-          Robotervermessungsdatenbank geschaffen werden, in der aufgezeichnete
-          Bewegungsdaten von Industrierobotern gespeichert werden können. Mit
-          einer umfänglichen Datenbank, die stets durch neue Messdaten erweitert
-          werden kann, können verschiedenste Anwendungen und Anwendungsszenarien
-          unterstützt werden, in denen dann zeit- und kostenintensive
-          individuelle Vermessungen vermieden werden.
-        </Typography>
-        <Typography as="p" className="mt-4">
-          Diese Website dient als Remote-Anwendung mit direkter Visualisierung
-          der Daten, die in der MongoDB-Atlas-Datenbank gespeichert sind.
+          </Link>{' '}
+          gefördert.
         </Typography>
       </div>
     </section>
